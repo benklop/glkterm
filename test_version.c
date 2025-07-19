08b43ea5ecd8340c2314ec1db5a73decc1cf16fa
Ben <benklop@gmail.com>
2025-07-19 13:07:18 -0400

    remove empty or migrated files

--- test_version.c
+++
@@ -1,9 +0,0 @@
-#include <ncurses.h>
-#include <stdio.h>
-int main() { 
-    printf("NCURSES_VERSION: %s\n", NCURSES_VERSION);
-    printf("NCURSES_VERSION_MAJOR: %d\n", NCURSES_VERSION_MAJOR);
-    printf("NCURSES_VERSION_MINOR: %d\n", NCURSES_VERSION_MINOR);
-    printf("NCURSES_VERSION_PATCH: %d\n", NCURSES_VERSION_PATCH);
-    return 0; 
-}
